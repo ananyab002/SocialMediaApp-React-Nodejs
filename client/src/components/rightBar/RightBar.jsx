0ff10ef9df8a67a0f268f8b22462c36552929e91
ananyab002 <ananyab002@gmail.com>
2024-05-07 12:26:45 +0200

    Right Bar design

--- client/src/components/rightBar/RightBar.jsx
+++ client/src/components/rightBar/RightBar.jsx
@@ -1,8 +1,86 @@
 import React from "react";
+import registerPage from '../images/registerPage.jpg';
+import './rightBar.scss'
 
 function RightBar() {
     return (
-        <div className="rightBar">RightBar</div>
+        <div className="rightBar">
+            <div className="container">
+                <div className="items">
+                    <span>Your Suggestions</span>
+                    <div className="user">
+                        <div className="userInfo">
+                        <img src={registerPage} />
+                            <span>Ananya</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                        <img src={registerPage} />
+                            <span>Ananya</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="items">
+                    <span>Latest Activities </span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={registerPage} />
+                            <p>
+                                <span>Ananya</span>    changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={registerPage} />
+                            <p>
+                                <span>Ananya</span>    changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={registerPage} />
+                            <p>
+                                <span>Ananya</span>    changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src={registerPage} />
+                            <p>
+                                <span>Ananya</span>    changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="items">
+                        <span>Online friends</span>
+                        <div className="user">
+                        <div className="userInfo">
+                            <img src={registerPage} />
+                            <div className="online"/>
+                                <span>Ananya</span>
+                        </div>
+                    </div>
+                    </div>
+            </div>
+        </div>
     )
 }
 export default RightBar;
