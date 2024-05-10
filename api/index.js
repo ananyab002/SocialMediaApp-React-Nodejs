e2ebddaff7db2753946920fb20e5c8cfc7a1f110
ananyab002 <ananyab002@gmail.com>
2024-05-10 10:46:56 +0200

    backend services login and register

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(8800, () => {
+  console.log("API working");
+});
